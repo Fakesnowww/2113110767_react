2137651b479c47730aaa7be4107e71529e82298e
Fakesnowww <boomtepch2546@gmail.com>
2023-06-24 11:21:08 +0700

    "JavaScript"

---
+++ javascriptFile/Backtrick.js
@@ -0,0 +1,11 @@
+let a = 2
+let b = 3
+let templatString = `the result of ${a} + ${b} is ${a + b}`
+
+let multilineString = ` First line
+    Second line
+    And third line
+`
+
+console.log(templatString)
+console.log(multilineString)
---
+++ javascriptFile/Function.js
@@ -0,0 +1,12 @@
+/* function avg (...args) {
+    let sum = 0
+    for (const item of args){
+        sum += item
+    }
+    return sum / args.length
+}
+
+console.log(avg(2, 3, 4, 5)) */
+
+let a = sum(2,3,4,5)
+console.log = a
