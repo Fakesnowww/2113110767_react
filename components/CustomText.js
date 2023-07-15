a73a4e1625ff8da6dd9e4aae3110a0ac3b256425
Fakesnowww <Boomtepch2546@gmail.com>
2023-07-15 12:15:10 +0700

    15/7/2023

---
+++ components/CustomText.js
@@ -0,0 +1,24 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MyCustomTextWith = ({ fname, lname }) => {
+  return (
+    <View>
+      <Text>{"\n"}</Text>
+      <Text></Text>
+        Your First Name is {fname}! and Last name is {lname}
+      </Text>
+    </View>
+  );
+};
+
+const CustomText = () => {
+  return (
+    <View style={{ alignItems: "center", top: 50 }}>
+      <MyCustomTextWith fname="Boonyasith" lname="Sawangroj" />
+      <MyCustomTextWith fname="Natthapum" lname="Kongsatjaviwat" />
+    </View>
+  );
+};
+
+export default CustomText;
