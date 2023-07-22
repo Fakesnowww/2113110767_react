2629a1d731d4a5978a0c902d86a2aabbc0efc4e8
Fakesnowww <Boomtepch2546@gmail.com>
2023-07-22 11:08:25 +0700

    "22-7-2023"

--- App.js
+++ App.js
@@ -1,26 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
-import ViewBoxesWithColorAndText from './components/ViewBoxesWithColorAndText';
-import DisplayandImage from './components/DisplayandImage';
-import LotsOfGreeting from './components/LotsOfGreeting';
-import CustomText from './components/CustomText';
+import { StyleSheet, Text, View } from "react-native";
+import ViewBoxesWithColorAndText from "./components/ViewBoxesWithColorAndText";
+// import DisplayandImage from "./components/DisplayandImage";
+// import LotsOfGreeting from "./components/LotsOfGreeting";
+// import Customtext from "./components/Customtext";
+import Counter from "./Counter";
+import Myinput from "./Myinput";
+import IncrementCounter from "./incrementCounter";
+import Form from "./Form";
 
 export default function App() {
   return (
-    <View>
-      {/*<ViewBoxesWithColorAndText/>       */}
-    {/* <DisplayandImage/> */}
-    {/* <LotsOfGreeting/> */}
-    <CustomText/>
+    <View style={styles.container}>
+      {/* <ViewBoxesWithColorAndText/> */}
+      {/*<DisplayandImage />*/}
+      {/*<LotsOfGreeting/>*/}
+      {/* <Customtext/> */}
+      {/* <Counter /> */}
+      {/* <Myinput/> */}
+      {/* <Form/> */}
+      <IncrementCounter/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-     flex:1,   
-     justifyContent:'center',    
-     alignItems:'center'   
-  }
- })
-
-
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
