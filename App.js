44ae140f5524402349257437caf6d114e7706af7
Fakesnowww <136876375+Fakesnowww@users.noreply.github.com>
2023-08-19 11:52:19 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import { StyleSheet, Text, View } from "react-native";
 // import Recap from "./components/Recap";
 import DefineStyle from "./components/DefineStyle";
 import RandomUsersScreen from "./components/RandomUsersScreen";
+import FlatList_Example1 from "./components/FlatList_Example1";
+import FlatList_HeaderFooter from "./FlatList_HeaderFooter";
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={{flex:1}}>
       {/* <DefineStyle/> */}/
-      <RandomUsersScreen/>
+      {/* <RandomUsersScreen/> */}
+      {/* <FlatList_Example1/> */}
+      <FlatList_HeaderFooter/>
     </View>
   );
 }
