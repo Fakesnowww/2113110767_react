0bb17171699337cb4df24fed3e82cdf53da8dd61
Fakesnowww <136876375+Fakesnowww@users.noreply.github.com>
2023-08-19 10:12:26 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,30 +1,24 @@
-
 import { StyleSheet, Text, View } from "react-native";
-import React  from "react";
-import UseEffectFlatList from "./components/useEffectFlatList";
-// import TouchablePractice from "./TouchablePractice";
-// import Example_useEffect from "./components/Example_useEffect";
-//sss
+// import Recap from "./components/Recap";
+import DefineStyle from "./components/DefineStyle";
+import RandomUsersScreen from "./components/RandomUsersScreen";
 
 
-const App =() =>{
-  return(
-    <View style={{flex:1 }}>
-      {/* // <TouchablePractice/> */}
-      {/* <Example_useEffect/> */}
-      <UseEffectFlatList/>
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <DefineStyle/> */}
+      <RandomUsersScreen/>
     </View>
-  )
+  );
 }
 
-export default App
 
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
-  }
-})
-
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
