f6a39201261a11d8fb02e07a009d99b6ce6bf448
Fakesnowww <136876375+Fakesnowww@users.noreply.github.com>
2023-09-09 09:29:29 +0700

    Add files via upload

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,16 +1,24 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from "react-native";
+import React from "react";
 
-const HomeScreen = ({navigation}) => {
-    return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-          <Text>Home Screen</Text>
-          <Button
-          title="Go to Details"
-          onPress={() => navigation.navigate('Details')}
-          />
-        </View>
-      );
-}
+const DetailsScreen = ({navigation}) => {
+    return(
+      <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+        <Text>Details Screen{'\n'}</Text>
+        <Button
+         title="Go to Details...Again"
+         onPress={()=>navigation.push('Details')}
+        />
+        <Button
+         title="Go to Home"
+         onPress={()=>navigation.navigate('Home')}
+        />
+        <Button
+         title="Go Back"
+         onPress={()=>navigation.goBack()}
+        />
+      </View>
+    )
+  }
 
-export default HomeScreen
+export default DetailsScreen;
